5b75be655158d5dd9b3a890a559d5226250d231d
Michael Timbs <hello@michaeltimbs.me>
2021-01-08 23:07:06 +1030

    use rollup to build package

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import pkg from './package.json';
+
+const extensions = ['.js', '.ts'];
+export default [
+// CommonJS (for Node) and ES module (for bundlers) build.
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+      },
+    ],
+    plugins: [
+      resolve({
+        extensions,
+      }),
+      babel({
+        exclude: 'node_modules/**',
+        extensions,
+      }),
+    ],
+  },
+];
